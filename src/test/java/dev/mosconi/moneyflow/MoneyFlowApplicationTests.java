bbd41d2d5136b3f6c86ec0906bb5f6ecfed8eb9a
Leonardo Mosconi <leomosconi97@gmail.com>
2025-11-24 23:07:04 -0300

    Feat - Início do desenvolvimento do projeto spring.

---
+++ src/test/java/dev/mosconi/moneyflow/MoneyFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mosconi.moneyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
